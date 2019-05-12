b2216985551bfbffe4752f1d3d7409fcf375e7f3
Cista_123 <47624059+Cista-123@users.noreply.github.com>
2019-05-12 22:19:02 +0200

    Add files via upload

--- task_24.py
+++ task_24.py
@@ -1,36 +1,71 @@
-def SzamRendszerEllenorzes (szamrendszer, szam):
-    if (szam[0] == '0' or szam[0] == 0) and szamrendszer == 10:
+def HelyesenVannakMegadvaASzamok (szamrendszer, elsoszam,masodikszam):
+    if szamrendszer < 2 or szamrendszer > 10:
+        print("Nem a megadott határok között van a számrendszer!")
+        return False
+    if (elsoszam[0] == '0' or elsoszam[0] == 0) and szamrendszer == 10:
+    #ha tízes számrendszer van megadva de nullával kezdődik
          print("0-val kezdődik de tízes számrendszer van megadva!")
+         exit()
          return False
-    for i in range (len(szam)):
-        if int(szam[i]) > szamrendszer-1:
-            print("Ez nem a megadott számrendszerben van reprezentálva!")
+    if (masodikszam[0] == '0' or masodikszam[0] == 0) and szamrendszer == 10:
+    #ha tízes számrendszer van megadva de nullával kezdődik
+         print("0-val kezdődik de tízes számrendszer van megadva!")
+         exit()
+         return False
+    segedvalt= str(elsoszam)
+    for i in range(len(segedvalt)):
+        if int(segedvalt[i]) >= szamRendszer:
+            print("A számban olyan elem található amely nagyobb mint maga a számrendszer engedi!")
+            return False
+    segedvalt = str(masodikszam)
+    for i in range(len(segedvalt)):
+        if int(segedvalt[i]) >= szamRendszer:
+            print("A számban olyan elem található amely nagyobb mint maga a számrendszer engedi!")
             return False
     return True
 
+def BaseNToBaseTen(b,num):#b= számrendszer num=szám
+    # Átvált N számrendsz.ből decim.-be
+    num = str(num)
+    n = len(str(num)) #számjegyek száma
+    sum = 0
+    szamHanyadikja = 0
+    for i in range(n-1, -1, -1):
+        #print(int(num[i]),"*",b,"**",i,"=",int(num[i])*b**i)
+        sum = sum + (int(num[szamHanyadikja])*b**i)
+        szamHanyadikja+=1
+        n -= 1
+    return sum #tízesben
 
 
+def BaseTenToBaseN(n,szamrendszer): #n:=decimal
+    #átvált decim.ből N számrendszerbe
+    m=""            #this will store the
+    while n != 0:
+        d = n // szamRendszer
+        r = n % szamRendszer
+        m = m + str(r)
+        n = d
+    seged = ""
+    for i in range(len(m)-1,-1,-1):
+        seged = seged+m[i]
+    return seged
 try :
     szamRendszer = int(input("Add meg a számrendszert!(egész számként, x >= 2 ^ x <= 10)"))
     elsoBeolvasas = input( "Kérem az első számot")
-    if not SzamRendszerEllenorzes(szamRendszer, elsoBeolvasas):
-        quit()
-    masodikBeolvasas = input( "Kérem a második számot")
-    if not SzamRendszerEllenorzes(szamRendszer, masodikBeolvasas):
+    masodikBeolvasas = input("Kérem a második számot")
+    if not HelyesenVannakMegadvaASzamok(szamRendszer, elsoBeolvasas,masodikBeolvasas):
         quit()
 except ValueError:
     print("Nem szám lett megadva!")
-
-
-
-
-
-"""
-
-kettohatvamy= 0
-for i in range (len(vmi),0,-1)
-	for y in range(0,len(vmi),1)
-		kettohatvany = kettohatvany + 2**y
-
-"""
-#for i in range(0,szamRendszer,1):
+if szamRendszer != 10:
+    elsoDecimalisban = BaseNToBaseTen(szamRendszer,elsoBeolvasas)
+    masodikDecimalisban = BaseNToBaseTen(szamRendszer,masodikBeolvasas)
+    print("Első decimálisban: ", elsoDecimalisban, "Második decimálisban: ", masodikDecimalisban)
+    osszeadasEredmenye = elsoDecimalisban+masodikDecimalisban
+    print("Összeadás eredménye 10-es számrendszerben: ", osszeadasEredmenye)
+    osszeadasEredmenye = BaseTenToBaseN(osszeadasEredmenye,szamRendszer)
+    print("Összeadás eredménye ",szamRendszer,"-es/as/ös/ számrendszerben: ",osszeadasEredmenye)
+    print("Visszaírva 10-esbe: ",BaseNToBaseTen(szamRendszer,osszeadasEredmenye))
+else:
+    print("Az összeadás eredménye", int(elsoBeolvasas)+int(masodikBeolvasas))
